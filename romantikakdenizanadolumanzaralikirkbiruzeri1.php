0dba005023289a368b5bf4221656f256b2592cf6
keremyarali <keremyarali@KEREM-MacBook-Air.local>
2015-05-03 16:30:39 +0300

    romantik akdeniz anadolu 41 üzeri

--- romantikakdenizanadolumanzaralikirkbiruzeri1.php
+++ romantikakdenizanadolumanzaralikirkbiruzeri1.php
@@ -11,11 +11,11 @@
     <nav class="nav">
         <img id="canberk-logo" src="images/Luogo.jpg" />
    <ul id="menu">
-            <li><a href="index1.html">Ana Sayfa</a>
+            <li><a href="index.html">Ana Sayfa</a>
             </li>
             <li><a href="hakkimizda.html">Hakkımızda</a>
             </li>
-            <li><a href="firstdisplay1.html">Hadi Bulalım!</a>
+            <li><a href="firstdisplay.html">Hadi Bulalım!</a>
             </li>
             <li><a href="restoranistek.html">Restoran ekle</a>
             
@@ -41,8 +41,21 @@
 <body bgcolor="#F1EBEB">
 <?php
 require('db.php');
-echo "<font face='Avenir Next'>Üzgünüz,seçimlerinize göre uygun restoran bulamadık";
+echo "<font face='Avenir Next'>Seçimlerinize göre aday restoranlar aşağıda listelenmiştir";
 $baglan=mysql_connect("localhost","root")or die(mysql_error());
 mysql_select_db("thesis",$baglan);
 
-?>
+$islema=mysql_query("SELECT kimle.kisibilgileri,tur.turbilgileri,ortam.ortambilgi,restoranreal.restoranadi,yer.isim,butce.butcearalik,url.url_add FROM restoranreal INNER JOIN kimle ON kimle.kim_id=restoranreal.kim_id INNER JOIN tur ON tur.tur_id=restoranreal.tur_id INNER JOIN ortam ON ortam.ortam_id=restoranreal.ortam_id INNER JOIN yer ON yer.yer_id=restoranreal.yer_id INNER JOIN butce ON butce.butce_id=restoranreal.butce_id INNER JOIN url ON url.url_id=restoranreal.url_id AND kimle.kim_id='4' AND ortam.ortam_id='1' AND tur.tur_id='7' AND yer.yer_id='34' AND butce.butce_id='12'");
+
+
+if($islema==FALSE){
+die(mysql_error());
+}
+
+
+while($row=mysql_fetch_array($islema)){
+$url=$row['url_add'];
+$restoranadi=$row['restoranadi'];
+echo '<br>';
+echo "<p><a href='$url'>$restoranadi</p></a>";
+}
