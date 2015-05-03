9ee3c5723d437de71a5e21e061900a545675e3a3
keremyarali <keremyarali@KEREM-MacBook-Air.local>
2015-05-03 16:50:18 +0300

    TR anadolu manzaralı 41

---
+++ tranadolumanzaralikirkbir.php
@@ -0,0 +1,14 @@
+<?php
+$con=mysql_connect("localhost","root","");
+if(!$con){
+die("Could not connect".mysql_error());
+}
+mysql_select_db("uyeler",$con);
+$result=mysql_query("SELECT * FROM androiddb");
+while($row=mysql_fetch_assoc($result))
+{
+$output[]=$row;
+}
+print(json_encode($output));
+mysql_close($con);
+?>
